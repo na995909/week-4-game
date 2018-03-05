71fcfdb99635b93322feb286eb98487cce01ae2a
Nelly Ackerman <pavel.ack@gmail.com>
2018-03-05 01:21:19 -0500

    crystal_game

---
+++ assets/javascript/game.js
@@ -0,0 +1,41 @@
+
+$(document).ready(function() {
+	init();
+
+	$(".btn").on("click", function() {
+		var res = parseInt($(this).val())+parseInt($(".scorebox").text());
+		$(".scorebox").text(res);
+		var rand = parseInt($(".greenbox").text());
+		if(res==rand) {
+			var a = parseInt($("#w").text());
+			$("#w").text(a+1);
+			init();
+		} else if(res>rand) {
+			var a = parseInt($("#l").text());
+			$("#l").text(a+1);
+			init();
+
+		}
+	}); 
+});
+
+
+function init() {
+
+	$(".greenbox").text(Math.floor(Math.random()*101+19));
+	$(".scorebox").text(0);
+	$(".btn").each(function() {
+		$(this).val(Math.floor(Math.random()*11+1));
+		console.log($(this).val());
+
+	});
+
+	/*$("#red").val(Math.floor(Math.random()*11+1));
+	$("#blue").val(Math.floor(Math.random()*11+1));
+	$("#yellow").val(Math.floor(Math.random()*11+1));
+	$("#green").val(Math.floor(Math.random()*11+1));
+	console.log($("#red").val());
+	console.log($("#blue").val());
+	console.log($("#yellow").val());
+	console.log($("#green").val()); */
+}
